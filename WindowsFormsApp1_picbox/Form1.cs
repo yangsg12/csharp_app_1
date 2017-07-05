a0ece5fc673c4713fa275372103db97f799eacf6
yang <yueyingyang12@gmail.com>
2017-07-05 21:20:07 +0800

    添加项目文件。

---
+++ WindowsFormsApp1_picbox/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1_picbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int picNo;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            picNo = 0;
+            timer1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "图文件|*jpg";
+            openFileDialog1.FilterIndex = 3;
+            openFileDialog1.ShowDialog();
+            listBox1.Items.Add(openFileDialog1.FileName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = picNo;
+            string s = listBox1.SelectedItem.ToString();
+
+            pictureBox1.Image = Image.FromFile(s);
+            picNo = picNo + 1;
+            if(picNo >= listBox1.Items.Count)
+            {
+                picNo = 0;
+            }
+        }
+    }
+}
---
+++ csharp_app_1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_app_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Point p = new Point(0, 240);
+            this.DesktopLocation = p;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point p = new Point(this.DesktopLocation.X + 1, this.DesktopLocation.Y + 1);
+            this.DesktopLocation = p;
+            if(p.X == 550)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = true;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Point p = new Point(this.DesktopLocation.X - 1, this.DesktopLocation.Y - 1);
+            this.DesktopLocation = p;
+            if(p.X == 150)
+            {
+                timer1.Enabled = true;
+                timer2.Enabled = false;
+            }
+        }
+    }
+}
